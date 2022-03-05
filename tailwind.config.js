d40dc2b70e5292895c88e9f2400faee2763283fe
ilhamdwikiarifani <ilhamdwiki0408@gmail.com>
2022-03-05 14:28:28 +0700

    asset--gallery

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      inter: ["Inter", "system-ui", "sans-serif"],
+    },
+  },
+  plugins: [],
+};
